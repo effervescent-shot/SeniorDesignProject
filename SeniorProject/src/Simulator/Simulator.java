13d98652a56ab44c765f7c2be4fb9a922aae271c
otugen <nhlezgiyctrk@gmail.com>
2018-03-25 15:32:51 +0300

    pair

--- SeniorProject/src/Simulator/Simulator.java
+++ SeniorProject/src/Simulator/Simulator.java
@@ -1,5 +1,15 @@
 package Simulator;
+import Network.*;
+import Helper.*;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Simulator {
+    Map<Integer, Node> networkNodes = new HashMap();
+    Map<Pair<Integer, Integer>, Link> newtworkLinks = new HashMap();
+
+
 
 }
